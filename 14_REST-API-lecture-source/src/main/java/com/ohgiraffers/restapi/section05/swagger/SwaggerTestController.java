1e8be3a5baa4d5b1bad52ebcf121f1d3cd9d3900
wdh970616 <wdh970616@naver.com>
2024-09-03 12:20:23 +0900

    rest api

--- 14_REST-API-lecture-source/src/main/java/com/ohgiraffers/restapi/section05/swagger/SwaggerTestController.java
+++ 14_REST-API-lecture-source/src/main/java/com/ohgiraffers/restapi/section05/swagger/SwaggerTestController.java
@@ -1,5 +1,10 @@
-package com.ohgiraffers.restapi.section02.responseentity;
+package com.ohgiraffers.restapi.section05.swagger;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,20 +20,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// Tag : API들의 그룹을 짓기 위한 어노테이션
+@Tag(name = "Spring boot Swagger 연동 API(USER 기능)")
 @RestController
-@RequestMapping("/entity")
-public class ResponseEntityTestController {
-
-    /*
-     * ResponseEntity
-     * 결과 데이터와 HTTP 상태 코드를 직접 제어할 수 있는 클래스
-     * HttpStatus, HttpHeaders, HttpBody를 포함하고있다.
-     * => 우리가 응답의 상태코드, 헤더, 바디를 좀 더 다양한 상황에 적절히 대응할 수 있게 해준다.
-     * */
+@RequestMapping("/swagger")
+public class SwaggerTestController {
 
     private List<UserDTO> users;
 
-    public ResponseEntityTestController() {
+    public SwaggerTestController() {
 
         users = new ArrayList<UserDTO>();
 
@@ -37,6 +37,8 @@ public class ResponseEntityTestController {
         users.add(new UserDTO(3, "user03", "pass03", "곰", LocalDate.now()));
     }
 
+    // user 정보 전체조회
+    @Operation(summary = "전체 회원 조회", description = "우리 사이트의 전체 회원 목록 조회")
     @GetMapping("/users")
     public ResponseEntity<ResponseMessage> findAllUsers() {
 
@@ -55,6 +57,14 @@ public class ResponseEntityTestController {
         return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
     }
 
+    // user 단일 조회
+    @Operation(
+            summary = "회원번호로 회원 조회", description = "회원번호를 통해 회원을 조회한다.",
+            parameters = {
+                    @Parameter(
+                            name = "userNo",
+                            description = "사용자 화면에서 넘어오는 user의 pk"
+                    )})
     @GetMapping("/users/{userNo}")
     public ResponseEntity<ResponseMessage> findUserByNumber(@PathVariable int userNo) {
 
@@ -110,6 +120,11 @@ public class ResponseEntityTestController {
     }
 
     // 유저 삭제
+    @Operation(summary = "회원 정보 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "회원정보 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못 입력된 파라미터")
+    })
     @DeleteMapping("/users/{userNo}")
     public ResponseEntity<?> removeUser(@PathVariable int userNo) {
 
